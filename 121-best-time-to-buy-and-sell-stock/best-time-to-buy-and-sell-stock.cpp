3b700a84941352e52c9e1ed272cd6bbcb4992d3a
Jiya gupta <jiyag3808@gmail.com>
2026-09-22 20:02:09 +0530

    Time: 3 ms (27.49%) | Memory: 97.2 MB (88.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = prices[0];
+        int minPrice = prices[0]; //by default pehle index wale ko minimum lenge fir aage compare krte jaenge 
         int maxProfit = 0;
 
-        for(int i = 1; i < prices.size(); i++) {
+        for(int i = 1; i < prices.size(); i++) { //isliye 1 se start kiya kyuki pehle index wake ko se to intitialise kr diya naa
             minPrice = min(minPrice, prices[i]);
             int profit = prices[i] - minPrice;
             maxProfit = max(maxProfit, profit);
